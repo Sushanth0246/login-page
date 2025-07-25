3627abf7b2c1ed86e9172747f201a16cdb4515e5
Rathishnaik17 <naikratish30@gmail.com>
2025-07-25 21:16:05 +0530

    firstcommit

--- signup/src/index.js
+++ signup/src/index.js
@@ -3,32 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-
-function txtfield(){
-  return (
-
-   <Box
-      component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-          ></TextField>
-       </div>
-    </Box>
-  );
-}
-   
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -36,8 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-export default txtfield;
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
